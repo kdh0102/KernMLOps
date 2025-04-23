05fd7d7c6ba182ab1d2878e5098c88d4c45d2351
Donghyun Kim <donghyun@utexas.edu>
2025-04-23 22:25:52 +0000

    Automated commit at 2025-04-23 22:25:52 from mew3

--- run_stress_ng_benchmarks.sh
+++ run_stress_ng_benchmarks.sh
@@ -31,6 +31,7 @@ create_yaml_file() {
 }
 
 dir_path="/KernMLOps/stress_ng_combinations"
+host_name=$(hostname)
 
 dirs=("$dir_path"/*/)
 total_dirs=${#dirs[@]}
@@ -50,11 +51,11 @@ for ((i = start_index; i < end_index && i < total_dirs; i++)); do
     cp -r "$dir" scripts/stress-ng-args
     file_count=$(find scripts/stress-ng-args -type f -name "*.txt" | wc -l)
     echo "File count: $file_count, Directory: $dir_name"
-    create_yaml_file "overrides.yaml" "starting_idx:0" "num_exps:$file_count" "num_reps:10"
+    create_yaml_file "overrides.yaml" "starting_idx:0" "num_exps:$file_count" "num_reps:1"
     make stress-ng-benchmarks
     mv data/curated data/$dir_name-curated
     zip -r "data/$dir_name-curated.zip" "data/$dir_name-curated"
-    scp "data/$dir_name-curated.zip" dhkim@mew3:/home/dhkim/kernmlops_results
+    scp "data/$dir_name-curated.zip" dhkim@mew3:/home/dhkim/kernmlops_results/$host_name-$dir_name-curated.zip
     rm -rf data/$dir_name-curated
     rm -rf data/$dir_name-curated.zip
     rm -rf tmp-stress-ng-*
